d984c74f8e276cb8cebbf934a5f50c07c75cabdd
ezzboyrazz <byrzzelif77@gmail.com>
2024-12-19 16:22:57 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -6,6 +6,5 @@ int main() {
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
 printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
-printf("Büyük kardeş küçük kardeşine sakin olmasını ve beraber olurlarsa başlarına kötü bir şey gelmeyeceğini söyledi");//Ömer Faruk Korkunç
-printf("Birden arkalarından gelen bir ses onları durdurdu."Kim var orada?"diye bağırdılar ama cevap alamadılar.Birbirlerine korkuyla daha sıkı sarıldılar.")//İrem Topsakal
+printf("Bir süre sese aldırış etmeden yürüyüp geçtiler.\n Hava kararmıştı, hem ısınmak hem de etrafı ışıtmak için kendilerine ateş yakmaları gerekiyordu bu yüzden çalı çırpı toplamaya başladılar.\n Yürüdükçe kapkaranlık ve soğuk mağaranın girişinde donakaldılar karşıdan bakan bir çift göz onları izliyordu.")//Elif Zeren Boyraz
 }
